61facb69f5d1cae4aa8e16e43dea826306c3df2c
olehskomorokha <oleg.skomoroha1@gmail.com>
2026-09-07 10:22:34 +0300

    add Search conference room model

---
+++ ConferenceRoomBooking.Service/Models/Etc/SearchConferenceRoomDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ConferenceRoomBooking.Service.Models.Etc;
+
+public class SearchConferenceRoomDto
+{
+    public int Capacity { get; set; }
+    public DateTime DateTime { get; set; }
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+}
