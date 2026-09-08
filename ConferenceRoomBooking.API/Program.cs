27f3f1a13cdc0d7c006fb7932b20b1089a066d69
olehskomorokha <oleg.skomoroha1@gmail.com>
2026-09-08 15:32:20 +0300

    add automatic database migrations on startup

--- ConferenceRoomBooking.API/Program.cs
+++ ConferenceRoomBooking.API/Program.cs
@@ -98,4 +98,10 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 app.UseHttpsRedirection();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.Run();
